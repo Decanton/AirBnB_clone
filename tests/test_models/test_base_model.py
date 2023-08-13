a7db9ad1e1c6bf29d6010ef9fe178dfd633c10ff
= <=>
2023-08-13 18:21:17 +0300

    Add test for unique id in test_base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -23,10 +23,30 @@ class TestBaseModel(TestCase):
         self.assertTrue(isinstance(obj.id, str))
         UUID(obj.id)
 
+    def test_unique(self):
+        """Unittest for unique ids"""
+        ob1 = BaseModel()
+        ob2 = BaseModel()
+        ob3 = BaseModel()
+        self.assertNotEqual(ob1.id, ob2.id)
+        self.assertNotEqual(ob2.id, ob3.id)
+        self.assertNotEqual(ob3.id, ob1.id)
+
+    def test_init_args(self):
+        """Unittest for creation with different arguments"""
+        ob1 = BaseModel('one')
+        ob2 = BaseModel('arg', kw='two')
+        ob3 = BaseModel(name='someone')
+        self.assertTrue(getattr(ob1, 'id', None) is not None)
+        self.assertTrue(getattr(ob2, 'id', None) is None)
+        self.assertTrue(getattr(ob3, 'id', None) is None)
+
     def test_to_dict(self):
         """Unitttest for BaseModel.to_dict"""
         obj = BaseModel()
-        self.assertTrue(isinstance(obj.to_dict(), dict))
+        objdict = obj.to_dict()
+        self.assertTrue(isinstance(objdict, dict))
+        self.assertEqual(objdict.get('__class__', None), 'BaseModel')
 
     def test_save(self):
         """Tests that updated_at is updated to now"""
